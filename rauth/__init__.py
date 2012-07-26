7e9dc271d930f5285438c456eef2aba7a2c3c370
Max Countryman <max.countryman@gmail.com>
2012-07-26 07:13:01 -0700

    removing convenience imports; fixes #20

--- rauth/__init__.py
+++ rauth/__init__.py
@@ -2,13 +2,9 @@
     rauth
     -----
 
-    OAuth 1.0/a and 2.0 wrapped around Python Requests.
+    OAuth 1.0/a, 2.0, and Ofly wrapped around Python Requests.
 '''
 
 __title__ = 'rauth'
-__version__ = '0.4.13'
+__version__ = '0.4.14'
 __license__ = 'MIT'
-
-from .service import OAuth1Service, OAuth2Service, OflyService
-
-services = (OAuth1Service, OAuth2Service, OflyService)
