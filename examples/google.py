d1c325438e8e14318bda1689006927349c6a0fcd
John-Kim Murphy <Leftium@gmail.com>
2012-10-16 17:31:52 +0900

    Fix redirect URI.
    This special URI tells Google to display a code with along instructions to
    copy-paste it.
    
    Added some instructional comments to help configure the Google API project
    properly. Also simplified the prompt for code (parameter).

--- examples/google.py
+++ examples/google.py
@@ -2,6 +2,14 @@ from rauth.service import OAuth2Service
 
 # Get a real consumer key & secret from:
 # https://code.google.com/apis/console/
+#
+# When creating a client id choose:
+#   - Application type: Installed application
+#   - Installed application type: Other
+#
+# For existing client id's, from "Edit settings":
+#   - Platform: Other
+
 google = OAuth2Service(
     name='google',
     authorize_url='https://accounts.google.com/o/oauth2/auth',
@@ -9,14 +17,11 @@ google = OAuth2Service(
     consumer_key='',
     consumer_secret='')
 
-redirect_uri = 'https://github.com/litl/rauth/'
+redirect_uri = 'urn:ietf:wg:oauth:2.0:oob'
 
 print 'Visit this URL in your browser: ' + google.get_authorize_url(redirect_uri=redirect_uri, scope='profile email')
 
-# This is a bit cumbersome, but you need to copy the code=something (just the
-# `something` part) out of the URL that's redirected to AFTER you login and
-# authorize the demo application
-code = raw_input('Enter code parameter (code=something) from URL: ')
+code = raw_input('Copy code from browser: ')
 
 # create a dictionary for the data we'll post on the get_access_token request
 data = dict(code=code, grant_type='authorization_code', redirect_uri=redirect_uri)
