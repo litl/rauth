950b7b643b32273f96dbeb0e23b0f1ec91348b39
John-Kim Murphy <john@leftium.com>
2012-11-03 17:23:39 +0900

    Fix bugs in facebook-cli.py example

--- examples/facebook-cli.py
+++ examples/facebook-cli.py
@@ -7,11 +7,11 @@ import webbrowser
 
 facebook = OAuth2Service(
     name='facebook',
-    authorize_url='https:/graph.facebook.com/oauth/authorize',
-    access_token_url='https:/graph.facebook.com/oauth/access_token',
+    authorize_url='https://graph.facebook.com/oauth/authorize',
+    access_token_url='https://graph.facebook.com/oauth/access_token',
     base_url='https://graph.facebook.com/',
-    consumer_key='YOUR CONSUMER KEY',
-    consumer_secret='YOUR CONSUMER SECRET')
+    consumer_key='YOUR_CONSUMER_KEY',
+    consumer_secret='YOUR_CONSUMER_SECRET')
 
 redirect_uri = 'https://www.facebook.com/connect/login_success.html'
 authorize_url = facebook.get_authorize_url(redirect_uri=redirect_uri,
@@ -22,7 +22,7 @@ print 'Visit this URL in your browser: ' + authorize_url
 webbrowser.open(authorize_url);
 
 url_with_code = raw_input("Copy URL from your browser's address bar: ")
-facebook.access_token = re.search('#access_token=([^&]*)', url_with_code).group(1)
+facebook.access_token = re.search('\#access_token=([^&]*)', url_with_code).group(1)
 
 user = facebook.get('me').content
 
