0084b77803e302b4b2cf127c7722028dd4aa6125
Max Countryman <max.countryman@gmail.com>
2012-11-13 17:04:56 -0800

    cleaning up a little

--- setup.py
+++ setup.py
@@ -24,21 +24,20 @@ if sys.argv[-1] == 'test':
     sys.exit(status)
 
 install_requires = ['requests>=0.12.0']
-if sys.version_info[1] < 7:
+if sys.version_info.major == 2 and sys.version_info.minor < 7:
     install_requires.append('unittest2>=0.5.1')
 
-setup(
-    name='rauth',
-    version=rauth.__version__,
-    description='A Python library for OAuth 1.0/a, 2.0, and Ofly.',
-    long_description=open('README.markdown').read(),
-    author='Max Countryman',
-    author_email='max@litl.com',
-    url='https://github.com/litl/rauth',
-    packages=find_packages(),
-    install_requires=install_requires,
-    license='MIT',
-    classifiers=(
+setup(name='rauth',
+      version=rauth.__version__,
+      description='A Python library for OAuth 1.0/a, 2.0, and Ofly.',
+      long_description=open('README.markdown').read(),
+      author='Max Countryman',
+      author_email='max@litl.com',
+      url='https://github.com/litl/rauth',
+      packages=find_packages(),
+      install_requires=install_requires,
+      license='MIT',
+      classifiers=(
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Programming Language :: Python',
@@ -46,6 +45,5 @@ setup(
         'Operating System :: OS Independent',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Software Development :: Libraries :: Python Modules',
-    ),
-    zip_safe=False,
-)
+        ),
+    zip_safe=False)
