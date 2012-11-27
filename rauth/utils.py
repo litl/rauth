072f41fabe97fceb6c6841662d353def8fcbf0d0
Max Countryman <max.countryman@gmail.com>
2012-11-27 08:02:49 -0800

    adding missing module

---
+++ rauth/utils.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+'''
+    rauth.utils
+    -----------
+
+    General utilities.
+'''
+
+from urlparse import parse_qsl
+
+
+def absolute_url(url):
+    return url.startswith(('http://', 'https://'))
+
+
+def parse_utf8_qsl(s):
+    d = dict(parse_qsl(s))
+
+    for k, v in d.items():
+        if isinstance(k, unicode) and isinstance(v, unicode):
+            # skip this iteration if we have no keys or values to update
+            continue
+        d.pop(k)
+        if not isinstance(k, unicode):
+            k = unicode(k, 'utf-8')
+        if not isinstance(v, unicode):
+            v = unicode(v, 'utf-8')
+        d[k] = v
+    return d
