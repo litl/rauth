e9a85b5954e2b18ba48059302ba89f2464450c4c
mickey06 <khalil.elkouhen@gmail.com>
2013-01-29 09:25:34 +0000

    Fix Key Error
    Add OAUTH 2.0 Constants
    Add webbrowser

--- examples/github.py
+++ examples/github.py
@@ -1,30 +1,39 @@
 from rauth.service import OAuth2Service
+import webbrowser
+from pprint import pprint
 
 # Get a real consumer key & secret from:
 # https://github.com/settings/applications/new
-github = OAuth2Service(
-    name='github',
-    authorize_url='https://github.com/login/oauth/authorize',
-    access_token_url='https://github.com/login/oauth/access_token',
-    consumer_key='8ae4946cc5a9af76f6d7',
-    consumer_secret='48aeb2b3c9226ae2b698eef4d7e6310473ccafa7')
 
-print 'Visit this URL in your browser: ' + github.get_authorize_url()
+NAME = 'github'
+AUTHORIZE_URL = 'https://github.com/login/oauth/authorize'
+ACCESS_TOKEN_URL = 'https://github.com/login/oauth/access_token'
+CLIENT_ID = ''
+CLIENT_SECRET = ''
+API = 'https://api.github.com/user'
+SCOPE = "repo user"
 
-# This is a bit cumbersome, but you need to copy the code=something (just the
-# `something` part) out of the URL that's redirected to AFTER you login and
-# authorize the demo application
+svc = OAuth2Service(name=NAME,
+                    authorize_url=AUTHORIZE_URL,
+                    access_token_url=ACCESS_TOKEN_URL,
+                    consumer_key=CLIENT_ID,
+                    consumer_secret=CLIENT_SECRET)
+
+authorize_url = svc.get_authorize_url(scope=SCOPE)
+
+print 'Visit this URL in your browser: ' + authorize_url
+webbrowser.open(authorize_url)
+
 code = raw_input('Enter code parameter (code=something) from URL: ')
 
 # create a dictionary for the data we'll post on the get_access_token request
-data = dict(code=code, redirect_uri='https://github.com/litl/rauth/')
+data = dict(code=code)
 
 # retrieve the access token
-resp = github.get_access_token('POST', data=data)
-access_token = resp.content['access_token']
+resp = svc.get_access_token('POST', data=data).content
+svc.access_token = resp['access_token']
 
 # make a request using the access token
-user = github.get('https://api.github.com/user',
-                  access_token=access_token).content
+user = svc.get(API).content
 
-print 'currently logged in as: ' + user['login']
+pprint(user)
