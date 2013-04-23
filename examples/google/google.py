facf045b8fae335bdd5cf5cfa05a444492da20af
Daniel Gonzalez (pegasus) <gonvaled@gonvaled.com>
2013-04-23 15:12:14 +0200

    Added flow to reuse access_token, if it has not yet expired. For the time being, the email address is hard-coded in GOOGLE_TEST_EMAIL

--- examples/google/google.py
+++ examples/google/google.py
@@ -26,7 +26,7 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///google.db'
 SECRET_KEY = '\xfb\x12\xdf\xa1@i\xd6>V\xc0\xbb\x8fp\x16#Z\x0b\x81\xeb\x16'
 DEBUG = True
 
-from secrets import GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_REDIRECT_URI
+from secrets import GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_REDIRECT_URI, GOOGLE_TEST_EMAIL
 
 # Flask setup
 app = Flask(__name__)
@@ -90,6 +90,18 @@ def login():
     }
     return redirect(google.get_authorize_url(**params))
 
+@app.route('/google/reuse')
+def reuse():
+    user = User.get(GOOGLE_TEST_EMAIL)
+    current = int(time.time())
+    if not user or user.expires_at < current :
+        redirect(url_for('login'))
+    # setup the session using the access_token
+    session = google.get_session(user.access_token)
+    # the user object as returned by google
+    user = session.get('https://www.googleapis.com/oauth2/v1/userinfo').json()
+    flash('Reused logging session as ' + user['email'])
+    return redirect(url_for('index'))
 
 @app.route('/google/authorized')
 def authorized():
@@ -100,7 +112,6 @@ def authorized():
 
     # make a request for the access token credentials using code
     redirect_uri = url_for('authorized', _external=True)
-    data = dict(code=request.args['code'], redirect_uri=redirect_uri)
     data = {
         'code': request.args['code'],
         'grant_type': 'authorization_code',
@@ -111,11 +122,15 @@ def authorized():
     access_token = response['access_token']
     expires_in = response['expires_in']
     expires_at = int(time.time()) + response['expires_in']
+
+    # setup the session using the access_token
     session = google.get_session(access_token)
 
-    # the "me" response
+    # the user object as returned by google
     user = session.get('https://www.googleapis.com/oauth2/v1/userinfo').json()
 
+    # create the user, save the access_token, and the expire_at, so that
+    # we can later verify if the access token is still valid
     User.get_or_create(user['email'], user['id'], response['access_token'], expires_at)
 
     flash('Logged in as ' + user['email'])
